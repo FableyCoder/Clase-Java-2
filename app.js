1374afb5d5a115a4f91d0f6592d485042ceed051
Fabley <simpsongamer@gmail.com>
2025-04-30 17:01:54 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+document.writeln("<h1> Clase 2 JavaScript </h1>")
+
+// crear arreglo con 20 numeros
+
+let arrayNumeros=[]
+
+for(let i=0;i<=20;i++){
+    arrayNumeros.push(i*2)
+}
+
+console.log(arrayNumeros)
+
+// Sumar items de un arreglo
+
